d299bc869a5231640aaf2f509ae12b51efc19d79
Steven Basart <xksteven@gmail.com>
2020-04-13 21:04:13 -0500

    add argparse to create_tasks

--- examples/ordinary_morality/create_tasks.py
+++ examples/ordinary_morality/create_tasks.py
@@ -1,8 +1,15 @@
 # simple task generator double check the max Assignments
 import argparse
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('hits', type=int, help='how many ordinary morality hits to submit')
+
+args = parser.parse_args()
+
 my_str = []
-for i in range(1,(1+500)//9): 
+# 9 is hard coded but it stands for the MAX_ASSIGNMENTS variable make sure it's less than 10
+# otherwise we take an additional 20% loss in fees
+for i in range(1,(1+args.hits)//9): 
     my_str.append("{\"a\": \""+str(i)+"\"}\n")
 with open("data.jsonl", "w") as f:
     for i in range(len(my_str)):
