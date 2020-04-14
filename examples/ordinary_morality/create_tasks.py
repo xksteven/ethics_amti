df770bc76df4cf3704667b2a2a270df03c98a54d
Steven Basart <xksteven@gmail.com>
2020-04-13 20:46:35 -0500

    adding latest batch to git

--- examples/ordinary_morality/create_tasks.py
+++ examples/ordinary_morality/create_tasks.py
@@ -1,8 +1,11 @@
 # simple task generator double check the max Assignments
+import argparse
+
 my_str = []
-for i in range(1,1+30): 
+for i in range(1,(1+500)//9): 
     my_str.append("{\"a\": \""+str(i)+"\"}\n")
 with open("data.jsonl", "w") as f:
     for i in range(len(my_str)):
         f.write(my_str[i])
 
+#sandbox qual ID = 3N5C8MI2ZB0D7EPGZHGD8HJCAK82GM
