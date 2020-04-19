f01e73c98522a56f4f0c7fd4e6fc30d3a6dc1a3b
Steven Basart <xksteven@gmail.com>
2020-04-19 10:11:01 -0500

    replace which batch with skip

--- examples/validate_ord_morality/create_tasks.py
+++ examples/validate_ord_morality/create_tasks.py
@@ -5,7 +5,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description='Create tasks to validate.')
 parser.add_argument('hits', type=int, default=10, help='how many validate ordinary morality hits to submit')
-parser.add_argument('which_batch', type=int, default=0, help='which batch to submit. Multiplies hits by this number to get.')
+parser.add_argument('--skip', type=int, default=0, help='from where in the dataset to submit.')
 parser.add_argument('--num_samples', type=int, default=4, help='')
 args = parser.parse_args()
 
@@ -19,7 +19,7 @@ gt_data = np.array(tmp)
 gt_indices = np.load("data_to_curate/gt_indices.npy")
 gt_data = gt_data[gt_indices]
 
-skip_amount = args.hits * args.which_batch
+skip_amount = args.skip
 gt_data = gt_data[skip_amount:]
 
 with open("data_to_curate/mturk_data.csv", "r") as f:
@@ -32,7 +32,7 @@ mturk_data = np.array(tmp)
 mturk_indices = np.load("data_to_curate/mturk_indices.npy")
 mturk_data = mturk_data[mturk_indices]
 
-skip_amount = (args.num_samples - 1) * args.hits * args.which_batch
+skip_amount = (args.num_samples - 1) * args.skip
 mturk_data = mturk_data[skip_amount:]
 
 upto = min(args.hits, len(mturk_data))
@@ -49,8 +49,8 @@ for index in range(0,(upto)):
         elif i == 2:
             tmp = gt_data[index]
             sent_to_insert = tmp[1]
-            assign_id0 = str(0) + str(tmp[0]) + str("ADan") + str(args.hits * args.which_batch + index)
-            assign_id1 = str(1) + str(tmp[0]) + str("ADan") + str(args.hits * args.which_batch + index)
+            assign_id0 = str(0) + str(tmp[0]) + str("ADan") + str(args.skip + index)
+            assign_id1 = str(1) + str(tmp[0]) + str("ADan") + str(args.skip + index)
         else:
             tmp = mturk_data[index*(args.num_samples-1) + i-1]
             sent_to_insert = tmp[1]
