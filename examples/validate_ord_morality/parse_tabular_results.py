425050068893db503f878763ac0ff9b45c48f92a
Steven Basart <xksteven@gmail.com>
2020-04-24 15:57:05 -0500

    ordinary morality validation

--- examples/validate_ord_morality/parse_tabular_results.py
+++ examples/validate_ord_morality/parse_tabular_results.py
@@ -19,21 +19,44 @@ with open(args.json, "r") as f:
 #"SubmitTime": "2020-04-18T10:23:30-05:00", "ApprovalTime": 1234,
 #"taskAnswers": "[{\"003JV9LGBJWTE1EIQ7L42EKE2YCLHOGD\":{\"wrong\":false},\"0131EUONYN2V3KWFG2AZ10KF58ZJVOVU\":{\"wrong\":true},\"013IOEN3P9S7JELXU03PVHWRC1PW7164\":{\"wrong\":true},\"01ADan83\":{\"wrong\":true},\"103JV9LGBJWTE1EIQ7L42EKE2YCLHOGD\":{\"not wrong\":true},\"1131EUONYN2V3KWFG2AZ10KF58ZJVOVU\":{\"not wrong\":false},\"113IOEN3P9S7JELXU03PVHWRC1PW7164\":{\"not wrong\":false},\"11ADan83\":{\"not wrong\":false}}]"}
 
-output_str = []
-for entry in data:
-    tmp = []
-    tmp.append("0")
-    tmp.append(entry["not_wrong"])
-    tmp.append(entry["WorkerId"])
-    tmp.append(entry["AssignmentId"])
-    output_str.append(tmp)
+#second index maps
+#0 is not wrong
+#1 is not wrong
+
+key_to_index = {"wrong": 0, "not wrong": 1}
+output = {}
+for i, entry in enumerate(data):
+    tmp = json.loads(entry["taskAnswers"])
+    #print(tmp, type(tmp))
+    for index, assignment in enumerate(tmp[0]):
+        #print(assignment, tmp[0])
+        wrong_id, ass_id = (assignment[1], assignment[2:])
+        wrong_or_not = list(tmp[0][assignment].keys())[0]
+        wrong_or_not_index = key_to_index[wrong_or_not]
+        wrong_or_not_sel = int(tmp[0][assignment][wrong_or_not])
+        tmp_ass = output.get(ass_id)
+        #if i > 2:
+        #    exit()
+        #print("\n\n", tmp[0], assignment)
+        #print(wrong_id, wrong_or_not, wrong_or_not_index, wrong_or_not_sel)
+        if not tmp_ass:
+            output[ass_id] = {wrong_id: [0,0]}
+        if not output[ass_id].get(wrong_id):
+            output[ass_id].update({wrong_id: [0,0]})
+        output[ass_id][wrong_id][wrong_or_not_index] += wrong_or_not_sel
+
+#TODO worker success rate
 
 save_name = args.save_path + args.json.split("/")[-2] + ".tsv"
 
 print(f"save_name = {save_name}")
 with open(save_name, "w") as f:
-    for entry in output_str:
-        tmp = "\t".join(entry)
-        tmp = tmp + "\n"
-        f.write(tmp)
-
+    for index, entry in enumerate(output):
+        for wr_or_not in output[entry]:
+            tmp = ""
+            tmp += entry + "\t"
+            tmp += (wr_or_not + "\t" + 
+                    str(output[entry][wr_or_not][0]) + "\t" +  str(output[entry][wr_or_not][1]))
+            tmp = tmp + "\n"
+            f.write(tmp)
+            #print(tmp,  "\n\n", output[entry])
