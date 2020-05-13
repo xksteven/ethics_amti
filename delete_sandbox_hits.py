9d1c96f6a9e028990607bc733519f71f8c7e5398
Steven Basart <xksteven@gmail.com>
2020-05-12 20:22:01 -0500

    updating the change pronouns form

--- delete_sandbox_hits.py
+++ delete_sandbox_hits.py
@@ -10,10 +10,13 @@ mturk=boto3.client('mturk',
         aws_secret_access_key="/fpxnAwum8sUG7Di2bACKtjQqv98r1rmqlCH+QnW",
         region_name='us-east-1',
         endpoint_url="https://mturk-requester-sandbox.us-east-1.amazonaws.com",
+        #endpoint_url="https://mturk-requester.us-east-1.amazonaws.com",
     )
 
 # Delete HITs
-for item in mturk.list_hits()['HITs']:
+#for item in mturk.list_hits()['HITs']:
+for item in mturk.list_hits_for_qualification_type(
+        QualificationTypeId="3YCLOEER7R8VN46A6TMH6U0503ROBY")['HITs']:
     hit_id=item['HITId']
     print('HITId:', hit_id)
 
@@ -38,25 +41,25 @@ for item in mturk.list_hits()['HITs']:
     item['assignments_submitted_count'] = len(assignments)
     answers = []
     print(assignments)
-    for assignment in assignments:
-    
-        # Retreive the attributes for each Assignment
-        worker_id = assignment['WorkerId']
-        assignment_id = assignment['AssignmentId']
-        try:
-            response = mturk.approve_assignment(
-                    AssignmentId=assignment_id,
-                       RequesterFeedback='string',
-                       OverrideRejection=True,
-                            )
-        except:
-            print("didn't approve")
-            pass
+    #for assignment in assignments:
+    #
+    #    # Retreive the attributes for each Assignment
+    #    worker_id = assignment['WorkerId']
+    #    assignment_id = assignment['AssignmentId']
+    #    try:
+    #        response = mturk.approve_assignment(
+    #                AssignmentId=assignment_id,
+    #                   RequesterFeedback='string',
+    #                   OverrideRejection=True,
+    #                        )
+    #    except:
+    #        print("didn't approve")
+    #        pass
 
-    # Delete the HIT
-    try:
-        mturk.delete_hit(HITId=hit_id)
-    except:
-        print('Not deleted')
-    else:
-        print('Deleted')
+    ## Delete the HIT
+    #try:
+    #    mturk.delete_hit(HITId=hit_id)
+    #except:
+    #    print('Not deleted')
+    #else:
+    #    print('Deleted')
