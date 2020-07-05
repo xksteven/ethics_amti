5e304616d03f6d87a52af7ffeb2a57225bda4dd9
collin-burns <cb3346@columbia.edu>
2020-07-05 13:28:29 -0700

    updated quetsion

--- examples/expert_val_justice/create_tasks.py
+++ examples/expert_val_justice/create_tasks.py
@@ -1,48 +1,44 @@
 # simple task generator double check the max Assignments
 import argparse
 import numpy as np
-
+import pandas as pd
+import json
+from sklearn.utils import shuffle
 
 parser = argparse.ArgumentParser(description='Create tasks to validate.')
-parser.add_argument('hits', type=int, default=360, help='how many validate ordinary morality hits to submit')
+parser.add_argument('--hits', type=int, default=100, help='how many hits to submit')
 parser.add_argument('-s', '--skip', type=int, default=0, help='from where in the dataset to submit.')
 parser.add_argument('--num_samples', type=int, default=10, help='number of samples used per hit')
 args = parser.parse_args()
 
-#with open("data_to_curate/mturk_data_bad.csv", "r") as f:
-with open("data_to_curate/test_justice_arxiv.tsv", "r") as f:  # start with test.
-    mturk_data = f.readlines()
+df = pd.read_csv("data_to_curate/test_justice_arxiv.tsv", sep="\t", header=None)
+df = df[args.skip:args.skip + args.num_samples*args.hits]
+# df = df[args.skip:args.skip + args.hits]
 
-tmp = []
-for i in range(len(mturk_data)):
-    tmp.append(mturk_data[i].strip().replace("\"","").split("\t")[1])
-mturk_data = np.array(tmp)
-print(mturk_data)
-print(len(mturk_data))
+savefile = "data.jsonl"
 
-skip_amount = (args.num_samples) * args.skip
-mturk_data = mturk_data[skip_amount:]
+class mydict(dict):
+    def __str__(self):
+        return json.dumps(self)
 
-upto = min(args.hits, len(mturk_data))
+# df = shuffle(df)  # to avoid pairs in the same batch
+list_of_dicts = []  # will save these dicts
+print(df)
 
-my_str = []
-for index in range(0,(upto)):
-    tmp_str = "{"
-    for i in range(args.num_samples):
-        # sent_to_insert = mturk_data[index*args.num_samples+i][1]
-        ex_idx = index*args.num_samples+i
-        sent_to_insert = mturk_data[ex_idx]
-        tmp_str += ("\"sent" + str(i) + "_idx\" : \"" + str(ex_idx) + "\",")
-        if i == (args.num_samples - 1):
-            tmp_str += ("\"sent"+str(i)+"\" : \"" + str(sent_to_insert) + "\"")
-        else:
-            tmp_str += ("\"sent"+str(i)+"\" : \"" + str(sent_to_insert) + "\",")
-        # tmp_str += ("\"good_" + str(i) + "\" : \"good_" + str(ex_idx) + "\",")
-        # tmp_str += ("\"bad_" + str(i) + "\" : \"bad_" + str(ex_idx) + "\",")
-        # tmp_str += ("\"low_quality_" + str(i) + "\" : \"low_quality_" + str(ex_idx) + "\",")
-    my_str.append(tmp_str+"}\n")
+for hit_num in range(args.hits):
+    start_idx = args.skip + args.num_samples*hit_num
+    d = {}
+    for j in range(args.num_samples):
+        idx = start_idx + j
+        sc = df.iloc[idx, 1]
+        d["sent{}".format(j)] = sc
+        d["good_{}".format(j)] = "good_{}".format(idx)
+        d["bad_{}".format(j)] = "bad_{}".format(idx)
+        d["low_quality_{}".format(j)] = "low_quality_{}".format(idx)
+
+    list_of_dicts.append(d)
 
 with open("data.jsonl", "w") as f:
-    for i in range(len(my_str)):
-        f.write(my_str[i])
+    for d in list_of_dicts:
+        f.write("{}\n".format(mydict(d)))
 
