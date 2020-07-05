b7867883046fbdaeb74d8cdf9d550f5a33702b70
collin-burns <cb3346@columbia.edu>
2020-07-05 14:01:17 -0700

    updated parse tabular results for validate_util

--- examples/validate_util/parse_tabular_results.py
+++ examples/validate_util/parse_tabular_results.py
@@ -14,15 +14,18 @@ with open(args.json, "r") as f:
     data = [json.loads(line.strip()) for line in tmp]
 
 results = {}
-df = pd.read_csv("data_to_curate/nonmanual_cleaning_util.tsv", sep="\t", header=None)
+# df = pd.read_csv("data_to_curate/nonmanual_cleaning_util.tsv", sep="\t", header=None)
+df = pd.read_csv("data_to_curate/arxiv_combined.tsv", sep="\t", header=None)
 
 KEY = {"ex1_better": -1, "unclear": 0, "ex2_better": 1}
 
 for d in data:
     answers = eval(d["taskAnswers"].replace("false", "False").replace("true", "True"))[0]
+    # print(answers)
     for key_num, key in enumerate(answers.keys()):
+        # print(key_num, key)
         idx = int(key.split("_")[0][3:])
-        j = int(key.split("_")[1][1:])
+        # print(idx)
 
         # get answer
         subdict = answers[key]
@@ -36,36 +39,33 @@ for d in data:
         else:
             continue
 
-        if (idx, j) in results.keys():
-            results[(idx, j)].append(ans)
+        if idx in results.keys():
+            results[idx].append(ans)
         else:
-            results[(idx, j)] = [ans]
+            results[idx] = [ans]
 
+results_by_idx = []
 good_idxs = []
-for idx in range(10000):
-    good = True
-    r = []
-    lengths, sums = [], []
-    for j in range(4):
-        key = (idx, j)
-        # assert key in results
-        if key not in results:
-            break
-        r += results[key]
-        lengths += [len(results[key])]
-        sums += [np.sum(results[key])]
-        if np.sum(np.array(results[key])) >= 0:
-        # if np.sum(results[key]) > -2:
-        # if np.sum(results[key] == -1) < 2:
-            good = False
-    if key not in results:
-        continue
-    print(idx, lengths, sums)
-    for j in range(5):
-        print(df.iloc[idx, j])
-
-    if good:
+for idx in results:
+    r = results[idx]
+    if np.sum(results[idx]) <= -2:
         good_idxs.append(idx)
 
 # print(results)
 print(good_idxs)
+
+# s1s = [df.iloc[idx, 0] for idx in good_idxs]
+# s2s = [df.iloc[idx, 1] for idx in good_idxs]
+idxs = [idx for idx in results]
+idxs = np.sort(idxs)
+s1s = [df.iloc[idx, 0] for idx in idxs]
+s2s = [df.iloc[idx, 1] for idx in idxs]
+labels = [results[idx] for idx in idxs]
+# workers = [df.iloc[idx, 2] for idx in good_idxs]
+# df = pd.DataFrame({"s1": s1s, "s2": s2s, "workers": workers})
+# df = pd.DataFrame({"s1": s1s, "s2": s2s})
+df = pd.DataFrame({"idxs": idxs, "s1": s1s, "s2": s2s, "labels": labels})
+print(len(df))
+save_name = args.json.split("/")[-2] + "_arxiv.tsv"
+# df.to_csv("arxiv_extra_nonexpert_cleaned_util.tsv", sep="\t", header=None, index=None)
+df.to_csv(save_name, sep="\t", header=None, index=None)
