b08a128d768e223f1ba038bb51afb3d467885396
collin-burns <cb3346@columbia.edu>
2020-07-05 11:12:07 -0700

    added old parse_tabular_results (to be changed)

--- examples/validate_util/parse_tabular_results.py
+++ examples/validate_util/parse_tabular_results.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import numpy as np
 import pickle
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="Parse the tabular data from Mturk and save to csv.")
 parser.add_argument("json", type=str, help="path to json file")
@@ -12,24 +13,20 @@ with open(args.json, "r") as f:
     tmp = f.readlines()
     data = [json.loads(line.strip()) for line in tmp]
 
-nidxs = 100
 results = {}
+df = pd.read_csv("data_to_curate/nonmanual_cleaning_util.tsv", sep="\t", header=None)
 
 KEY = {"ex1_better": -1, "unclear": 0, "ex2_better": 1}
 
 for d in data:
     answers = eval(d["taskAnswers"].replace("false", "False").replace("true", "True"))[0]
-    print(answers)
-    print("")
-    for key in answers.keys():
-        # print(key)
+    for key_num, key in enumerate(answers.keys()):
         idx = int(key.split("_")[0][3:])
         j = int(key.split("_")[1][1:])
-        if "ex2" in key:
-            j -= 1
 
         # get answer
         subdict = answers[key]
+        # print("subdict", subdict)
         if "unclear" in subdict and True in subdict.values():  # only one item
             ans = KEY["unclear"]
         elif "ex1" in key and True in subdict.values():
@@ -44,22 +41,28 @@ for d in data:
         else:
             results[(idx, j)] = [ans]
 
-        # print(idx, j, ans)
-
 good_idxs = []
-for idx in range(nidxs):
+for idx in range(10000):
     good = True
     r = []
+    lengths, sums = [], []
     for j in range(4):
         key = (idx, j)
-        assert key in results
+        # assert key in results
+        if key not in results:
+            break
         r += results[key]
-        if np.sum(np.array(results[key]) > -1) > 0:
+        lengths += [len(results[key])]
+        sums += [np.sum(results[key])]
+        if np.sum(np.array(results[key])) >= 0:
         # if np.sum(results[key]) > -2:
         # if np.sum(results[key] == -1) < 2:
             good = False
-    if idx in [0, 1, 2, 3, 4]:
-        print(r)
+    if key not in results:
+        continue
+    print(idx, lengths, sums)
+    for j in range(5):
+        print(df.iloc[idx, j])
 
     if good:
         good_idxs.append(idx)
